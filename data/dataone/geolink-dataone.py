84af2cfbad870185c66d7ec9d2d05089ea5c9e9f
Bryce Mecum <petridish@gmail.com>
2015-08-06 15:39:54 -0800

    Reorganize getDataList function body

--- data/dataone/geolink-dataone.py
+++ data/dataone/geolink-dataone.py
@@ -8,11 +8,11 @@
 
 def getDataList(page, pagesize):
     start = (page-1)*pagesize
-    d1query = "https://cn.dataone.org/cn/v1/query/solr/?fl=identifier,title,abstract,author,authorLastName,origin,submitter,rightsHolder,documents,resourceMap,authoritativeMN&q=formatType:METADATA+AND+(datasource:*LTER+OR+datasource:*KNB+OR+datasource:*PISCO+OR+datasource:*GOA)+AND+-obsoletedBy:*&rows="+str(pagesize)+"&start="+str(start)
-    #d1query = "https://cn.dataone.org/cn/v1/query/solr/?fl=identifier,title,abstract,author,authorLastName,origin,submitter,rightsHolder,documents,resourceMap,authoritativeMN&q=formatType:METADATA+AND+-obsoletedBy:*&rows="+str(pagesize)+"&start="+str(start)
-    res = urllib2.urlopen(d1query)
-    content = res.read()
-    xmldoc = ET.fromstring(content)
+    fields = ",".join(["identifier","title","abstract","author","authorLastName","origin","submitter","rightsHolder","documents","resourceMap","authoritativeMN","obsoletes"])
+    
+    d1query = "https://cn.dataone.org/cn/v1/query/solr/?fl=" + fields + "&q=identifier:*dpennington*+AND+formatType:METADATA+AND+(datasource:*LTER+OR+datasource:*KNB+OR+datasource:*PISCO+OR+datasource:*GOA)+AND+-obsoletedBy:*&rows="+str(pagesize)+"&start="+str(start)
+    xmldoc = getXML(d1query)
+
     return(xmldoc)
 
 def getXML(url):
