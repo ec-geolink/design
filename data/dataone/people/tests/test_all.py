5e929b742dd251fd8236e52e7ae39c59ea38d3b7
Bryce Mecum <petridish@gmail.com>
2015-08-18 16:30:21 -0800

    Adjust tests to match the way the program works now

--- data/dataone/people/tests/test_all.py
+++ data/dataone/people/tests/test_all.py
@@ -13,20 +13,18 @@ def test_same_email_different_name():
     j = job.Job("tests/002_same_email_different_name")
     j.run()
 
-    assert len(j.people) == 1
+    assert len(j.people) == 2
 
 
 def test_can_process_eml_user_id():
     j = job.Job("tests/003_can_process_eml_user_id")
     j.run()
 
-    user = j.people[0]
-
-    assert len(user["user_id"]) == 4
+    assert len(j.people) == 1
 
 
 def test_can_deal_with_middle_names():
     j = job.Job("tests/004_can_deal_with_middle_names")
     j.run()
 
-    assert len(j.people) == 2
+    assert len(j.people) == 4
