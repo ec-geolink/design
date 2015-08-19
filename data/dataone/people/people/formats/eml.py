5de83323d290491c88c0ed7ddb8fa01d0877c7d0
Bryce Mecum <petridish@gmail.com>
2015-08-18 16:29:06 -0800

    Change EML format processor so it doesn't do dup checking

--- data/dataone/people/people/formats/eml.py
+++ data/dataone/people/people/formats/eml.py
@@ -3,6 +3,11 @@
     Processing functions for processing eml
 """
 
+# TODO: Get more fields from EML spec, like address
+# TODO: Check out whether this stuff changes over spec versions
+# TODO: Check if the eml- subfiles need to be processed
+
+
 import re
 from people import find
 
@@ -23,201 +28,81 @@ def process(job, xmldoc, document):
 
 
 def processCreator(job, creator, document):
-    """Process the <creator> tag in an EML document"""
-
-    if creator.find("./individualName") is not None:
-        person = processCreatorIndividual(job,
-                                          creator,
-                                          document)
-
-        processPerson(job, person, document)
-
-    elif creator.find("./organizationName") is not None:
-        organization = processCreatorOrganization(job,
-                                                  creator,
-                                                  document)
-
-        processOrganization(job, organization, document)
-
-
-def processPerson(job, person, document):
-    """ Processes a parsed EML creator that is an individual"""
-
-    if not person:
-        return
-
-    match = find.findPerson(job, person)
-
-    if match == -1:
-        person["documents"] = [str(document)]
-
-        job.people.append(person)
-    else:
-        existing = job.people[match]
-
-        if "documents" in existing:
-            existing["documents"].append(str(document))
-        else:
-            existing["documents"] = [str(document)]
-
-
-def processOrganization(job, organization, document):
-    """ Processes a parsed EML creator that is an organization"""
-
-    if not organization:
-        return
-
-    match = find.findOrganization(job, organization)
-
-    if match == -1:
-        organization["documents"] = [str(document)]
-
-        job.organizations.append(organization)
-    else:
-        existing = job.organizations[match]
-
-        if "documents" in existing:
-            existing["documents"].append(str(document))
-        else:
-            existing["documents"] = [str(document)]
-
-
-def processCreatorIndividual(job, creator, document):
     """ Proccess a <creator> tag for an individual.
-
-        Processing involves two steps:
-
-            1. Parsing relevant information (e.g. name)
-            2. Scoring that information against existing records.
     """
 
-    person = {}
+    record = {}
 
     individual = creator.find("./individualName")
+    organization = creator.find("./organizationName")
 
     if individual is not None:
-        salutation = individual.find("./salutation")
-        given_name = individual.find("./givenName")
-        sur_name = individual.find("./surName")
+        record = processIndividual(record, individual)
 
-        person = processSalutation(person, salutation)
-        person = processGivenName(person, given_name)
-        person = processSurName(person, sur_name)
+    if organization is not None:
+        record['eml_org'] = organization.text
+
+    address = creator.find("./address")
+
+    if address is not None:
+        record = processAddress(record, address)
 
     user_id = creator.find("./userId")
-    person = processUserId(person, user_id)
-
-    email = creator.find("./electronicMailAddress")
-    person = processEmail(person, email)
-
-    return person
-
-
-def processCreatorOrganization(job, creator, document):
-    """ Proccess a <creator> tag for an organization.
-
-        Processing involves two steps:
-
-            1. Parsing relevant information (e.g. name)
-            2. Scoring that information against existing records.
-    """
-    organization = {}
-
-    name = creator.find("./organizationName")
-    email = creator.find("./electronicMailAddress")
-    url = creator.find("./onlineUrl")
-
-    if name is not None:
-        organization["name"] = name.text.lower()
-
-    if email is not None:
-        organization["email"] = email.text.lower()
-
-    if url is not None:
-        organization["url"] = url.text.lower()
-
-    return organization
-
-
-def processSalutation(person, salutation):
-    """
-    Adds information from the salutation to the current person.
-    """
-
-    if salutation is not None:
-        title = salutation.text.lower()
-        person["title"] = title.translate(None, ".")
-
-    return person
-
-
-def processGivenName(person, given_name):
-    """
-    Adds information from the given name to the current person.
-    """
-
-    if given_name is not None:
-        first_name = given_name.text.lower()
-
-        # First I.
-        pattern_one = re.compile("^\w+\s+\w{1}\.?$")
-
-        # First I. J.
-        pattern_two = re.compile("^\w+\s+\w{1}\.?\s?\w{1}\.?$")
-
-        if pattern_one.match(first_name):
-            first_name = first_name.split(" ")
-
-            person["first"] = first_name[0]
-        elif pattern_two.match(first_name):
-            first_name = first_name.split(" ")
-
-            person["first"] = first_name[0]
-        else:
-            person["first"] = first_name
-
-    return person
-
-
-def processSurName(person, sur_name):
-    """
-    Adds information from the sur name to the current person.
-    """
-
-    if sur_name is not None:
-        person["last"] = sur_name.text.lower()
-
-    return person
-
-
-def processUserId(person, user_id):
-    """
-    Adds information from the user id to the current person.
-    """
 
     if user_id is not None:
-        user_id_fields = re.compile("(\w+=\w+)+").findall(user_id.text)
+        record['eml_user_id'] = user_id.text
 
-        if len(user_id_fields) > 0:
-            fields = []
-
-            for field in user_id_fields:
-                k, v = field.split("=")
-
-                if k and v:
-                    fields.append({k.lower(): v.lower()})
-
-            person["user_id"] = fields
-
-    return person
-
-
-def processEmail(person, email):
-    """
-    Adds information from the email to the current person.
-    """
+    email = creator.find("./electronicMailAddress")
 
     if email is not None:
-        person["email"] = email.text.lower()
+        record['eml_email'] = email.text
 
-    return person
+    phone = creator.find("./phone[@phonetype='voice']")
+
+    if phone is not None:
+        record['eml_phone'] = phone.text
+
+    record['document'] = document
+    
+    if individual is None:
+        job.organizations.append(record)
+    else:
+        job.people.append(record)
+
+
+def processIndividual(record, individual):
+    salutation = individual.find("./salutation")
+    sur_name = individual.find('./surName')
+    given_name = individual.find('./givenName')
+
+    if salutation is not None:
+        record["eml_title"] = salutation.text
+
+    if given_name is not None:
+        record["eml_last"] = given_name.text
+
+    if sur_name is not None:
+        record["eml_first"] = sur_name.text
+
+    return record
+
+
+def processAddress(record, address):
+    delivery_point = address.find("./deliveryPoint")
+    city = address.find("./city")
+    admin_area = address.find("./administrativeArea")
+    postal = address.find("./postalCode")
+
+    if delivery_point is not None:
+        record['eml_delivery_point'] = delivery_point.text
+
+    if city is not None:
+        record['eml_city'] = city.text
+
+    if admin_area is not None:
+        record['eml_admin_area'] = admin_area.text
+
+    if postal is not None:
+        record['eml_postal'] = postal.text
+
+    return record
