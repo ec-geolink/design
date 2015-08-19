8c4bfa9a89182c04284dd99bddae3c5587cd0d5f
Bryce Mecum <petridish@gmail.com>
2015-08-18 16:30:43 -0800

    Update the demo script for the new program structure

--- data/dataone/people/demo/demo.py
+++ data/dataone/people/demo/demo.py
@@ -5,12 +5,53 @@
 """
 
 from people import job
+import unicodecsv
 
 
 def main():
     j = job.Job("./documents")
     j.run()
     j.summary()
+
+    print "Writing to CSV"
+
+    # The column headers that are available are varied so we'll process
+    # them beforehand to set them up
+    saveCollection("demo/people.csv", j.people)
+    saveCollection("demo/organizations.csv", j.organizations)
+
+
+def saveCollection(filename, collection):
+    column_names = ["Id"]
+
+    for item in collection:
+        for field in item:
+            if field not in column_names:
+                column_names.append(field)
+
+    column_names = sorted(column_names)
+
+    with open(filename, "wb") as f:
+        writer = unicodecsv.writer(f, encoding='utf-8')
+        writer.writerow(column_names)
+
+        row_id = 0
+
+        for item in collection:
+            row = []
+            row.append(str(row_id))
+
+            for column in column_names:
+                if column in item:
+                    row.append(item[column])
+                else:
+                    if column != "Id":
+                        row.append("")
+
+            writer.writerow(row)
+
+            row_id += 1
+
 
 if __name__ == "__main__":
     main()
