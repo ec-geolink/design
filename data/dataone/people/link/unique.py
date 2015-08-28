d6223983f8ac0324c6b7e11bcc1dad37ddd36b96
Bryce Mecum <petridish@gmail.com>
2015-08-27 17:07:24 -0800

    Rename unique script to unique.py

--- data/dataone/people/link/unique.py
+++
@@ -1,6 +0,0 @@
-    people_field_names = ["Id", "full_name", "salutation", "first_name",
-                          "last_name", "organization", "address", "email",
-                          "phone", "document", "type", "source", "format"]
-
-    organization_field_names = ["Id", "name", "address", "email", "phone",
-                                "document", "type", "source", "format"]
